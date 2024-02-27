243398212bf15843dd4fdc4c75fd26ef1dc88a63
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-02-27 14:46:55 +0100

    Bruker k9abakus som db navn for lokal kjøring som er lik det som k9-verdikjede bruker. (#21)

--- lokalutvikling/postgres-init/k9abakus.sql
+++ lokalutvikling/postgres-init/k9abakus.sql
@@ -4,8 +4,8 @@ GRANT ALL ON DATABASE k9_abakus_unit TO k9_abakus_unit;
 ALTER DATABASE k9_abakus_unit SET timezone TO 'Europe/Oslo';
 ALTER DATABASE k9_abakus_unit OWNER TO k9_abakus_unit;
 
-CREATE DATABASE k9_abakus;
-CREATE USER k9_abakus WITH PASSWORD 'k9_abakus';
-GRANT ALL ON DATABASE k9_abakus TO k9_abakus;
-ALTER DATABASE k9_abakus SET timezone TO 'Europe/Oslo';
-ALTER DATABASE k9_abakus OWNER TO k9_abakus;
+CREATE DATABASE k9abakus;
+CREATE USER k9abakus WITH PASSWORD 'k9abakus';
+GRANT ALL ON DATABASE k9abakus TO k9abakus;
+ALTER DATABASE k9abakus SET timezone TO 'Europe/Oslo';
+ALTER DATABASE k9abakus OWNER TO k9abakus;
