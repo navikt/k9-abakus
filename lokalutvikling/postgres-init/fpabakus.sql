bdcb1672e342997049ffa25b1247149da86cbc5c
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-02-27 12:35:15 +0100

    Fikser oppsett for lokal kjøring. (#17)

--- lokalutvikling/postgres-init/fpabakus.sql
+++ lokalutvikling/postgres-init/fpabakus.sql
@@ -4,8 +4,8 @@ GRANT ALL ON DATABASE k9_abakus_unit TO k9_abakus_unit;
 ALTER DATABASE k9_abakus_unit SET timezone TO 'Europe/Oslo';
 ALTER DATABASE k9_abakus_unit OWNER TO k9_abakus_unit;
 
-CREATE DATABASE k9-abakus;
-CREATE USER k9-abakus WITH PASSWORD 'k9-abakus';
-GRANT ALL ON DATABASE k9-abakus TO 'k9-abakus';
-ALTER DATABASE k9-abakus SET timezone TO 'Europe/Oslo';
-ALTER DATABASE k9-abakus OWNER TO 'k9-abakus';
+CREATE DATABASE k9_abakus;
+CREATE USER k9_abakus WITH PASSWORD 'k9_abakus';
+GRANT ALL ON DATABASE k9_abakus TO k9_abakus;
+ALTER DATABASE k9_abakus SET timezone TO 'Europe/Oslo';
+ALTER DATABASE k9_abakus OWNER TO k9_abakus;
