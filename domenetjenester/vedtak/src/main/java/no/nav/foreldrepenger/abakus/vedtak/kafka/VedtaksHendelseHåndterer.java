7455384b04899febec37575106cf69769d8fccd4
Michal J. Sladek <michal.sladek@nav.no>
2024-03-01 12:54:58 +0100

    Slå av lagring av vedtak en så lenge man går mot den samme databasen

--- domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/abakus/vedtak/kafka/VedtaksHendelseHåndterer.java
+++ domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/abakus/vedtak/kafka/VedtaksHendelseHåndterer.java
@@ -5,6 +5,8 @@ import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,20 +21,26 @@ public class VedtaksHendelseHåndterer {
 
     private static final Logger LOG = LoggerFactory.getLogger(VedtaksHendelseHåndterer.class);
     private ProsessTaskTjeneste taskTjeneste;
+    private boolean lagreVedtak;
 
     public VedtaksHendelseHåndterer() {
         // CDI
     }
 
     @Inject
-    public VedtaksHendelseHåndterer(ProsessTaskTjeneste taskTjeneste) {
+    public VedtaksHendelseHåndterer(@KonfigVerdi(value = "kafka.lagre.vedtak", defaultVerdi = "true") Boolean lagreVedtak,
+                                    ProsessTaskTjeneste taskTjeneste) {
         this.taskTjeneste = taskTjeneste;
+        this.lagreVedtak = lagreVedtak;
     }
 
     void handleMessage(String key, String payload) {
         LOG.debug("Mottatt ytelse-vedtatt hendelse med key='{}', payload={}", key, payload);
-        var data = ProsessTaskDataBuilder.forProsessTask(LagreVedtakTask.class).medProperty(LagreVedtakTask.KEY, key).medPayload(payload);
-
-        taskTjeneste.lagre(data.build());
+        if (lagreVedtak) {
+            var data = ProsessTaskDataBuilder.forProsessTask(LagreVedtakTask.class).medProperty(LagreVedtakTask.KEY, key).medPayload(payload);
+            taskTjeneste.lagre(data.build());
+        } else {
+            LOG.info("Lagring av vedtak er slått av. Husk å slå det på igjen samtidig med k9-abakus flyttes til egen database.");
+        }
     }
 }
