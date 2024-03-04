2d0c5e0bc322b741f33850a542a1f407a6528f99
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-03-04 14:54:20 +0100

    Revert "Fix sql"
    This reverts commit 3bac2ea0941ad5cb6655932b9717ae1bde96341b.

--- migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_029__fjern_timer.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_029__fjern_timer.sql
@@ -1,2 +1,2 @@
 alter table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer;
-alter table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer_fulltid;
+alte table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer_fulltid;
