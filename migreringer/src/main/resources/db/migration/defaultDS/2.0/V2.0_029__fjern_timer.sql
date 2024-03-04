b82de0924ca6b99e04ebcbd28cca8824c9ff3225
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-03-04 14:34:05 +0100

    Legger til sjekk på om kolonne finnes

--- migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_029__fjern_timer.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_029__fjern_timer.sql
@@ -1,1 +1,2 @@
-alter table IAY_AKTIVITETS_AVTALE drop column antall_timer, drop column antall_timer_fulltid;
+alter table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer;
+alte table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer_fulltid;
