3bac2ea0941ad5cb6655932b9717ae1bde96341b
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-03-04 14:35:16 +0100

    Fix sql

--- migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_029__fjern_timer.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/2.0/V2.0_029__fjern_timer.sql
@@ -1,2 +1,2 @@
 alter table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer;
-alte table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer_fulltid;
+alter table IAY_AKTIVITETS_AVTALE drop column if exists antall_timer_fulltid;
