2b567c6d36728981acd9f6a998d50bc2298816dd
Espen Velsvik <espenjv@users.noreply.github.com>
2024-04-08 13:17:36 +0200

    Revert af0e6ae90cecfaebed334437453645f0cd974c5d (#93)

---
+++ domenetjenester/iay/src/main/java/no/nav/k9/abakus/registerdata/inntekt/sigrun/klient/summertskattegrunnlag/SigrunSummertSkattegrunnlagResponse.java
@@ -0,0 +1,8 @@
+package no.nav.k9.abakus.registerdata.inntekt.sigrun.klient.summertskattegrunnlag;
+
+import java.time.Year;
+import java.util.Map;
+import java.util.Optional;
+
+public record SigrunSummertSkattegrunnlagResponse(Map<Year, Optional<SSGResponse>> summertskattegrunnlagMap) {
+}
