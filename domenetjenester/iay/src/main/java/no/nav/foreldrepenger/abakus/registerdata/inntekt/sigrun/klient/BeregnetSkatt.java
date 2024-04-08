af0e6ae90cecfaebed334437453645f0cd974c5d
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-04-08 11:59:07 +0200

    Bruk PGI isf BS/SSG (#83)
    * Bruk PGI isf BS/SSG
    
    * Tilfelle av 500 som er not found i kilden
    
    * Sigrun skal håndtere PGIF008
    
    * Riktig konfig for sigrun

--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/klient/BeregnetSkatt.java
+++
@@ -1,4 +0,0 @@
-package no.nav.k9.abakus.registerdata.inntekt.sigrun.klient;
-
-public record BeregnetSkatt(String tekniskNavn, String verdi) {
-}
--- domenetjenester/iay/src/main/java/no/nav/foreldrepenger/abakus/registerdata/inntekt/sigrun/klient/summertskattegrunnlag/SSGResponse.java
+++
@@ -1,6 +0,0 @@
-package no.nav.k9.abakus.registerdata.inntekt.sigrun.klient.summertskattegrunnlag;
-
-import java.util.List;
-
-public record SSGResponse(List<SSGGrunnlag> grunnlag, List<SSGGrunnlag> svalbardGrunnlag, String skatteoppgjoersdato) {
-}
