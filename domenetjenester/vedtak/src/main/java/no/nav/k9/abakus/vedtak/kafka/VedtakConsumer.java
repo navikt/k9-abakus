8c10900ef6bb93fc2a645538703aa3766f54f9db
Espen Velsvik <espenjv@users.noreply.github.com>
2024-04-08 14:18:40 +0200

    Revert "Felles 708 (#62)" (#94)
    This reverts commit 2904502a9804c89f499733e1544c47e689a2dee2.

--- domenetjenester/vedtak/src/main/java/no/nav/k9/abakus/vedtak/kafka/VedtakConsumer.java
+++ domenetjenester/vedtak/src/main/java/no/nav/k9/abakus/vedtak/kafka/VedtakConsumer.java
@@ -1,13 +1,22 @@
 package no.nav.k9.abakus.vedtak.kafka;
 
-import java.util.List;
+import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import no.nav.vedtak.felles.integrasjon.kafka.KafkaConsumerManager;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaProperties;
 import no.nav.vedtak.log.metrics.Controllable;
 import no.nav.vedtak.log.metrics.LiveAndReadinessAware;
 
@@ -16,19 +25,47 @@ public class VedtakConsumer implements LiveAndReadinessAware, Controllable {
 
     private static final Logger LOG = LoggerFactory.getLogger(VedtakConsumer.class);
 
-    private KafkaConsumerManager<String, String> kcm;
+    private static final String APPLICATION_ID = "k9-abakus"; // Hold konstant pga offset commit
+
+    private KafkaStreams stream;
+    private String topic;
 
     VedtakConsumer() {
     }
 
     @Inject
-    public VedtakConsumer(VedtaksHendelseHåndterer vedtaksHendelseHåndterer) {
-        this.kcm = new KafkaConsumerManager<>(List.of(vedtaksHendelseHåndterer));
+    public VedtakConsumer(@KonfigVerdi(value = "kafka.fattevedtak.topic", defaultVerdi = "teamforeldrepenger.familie-vedtakfattet-v1") String topicName,
+                          VedtaksHendelseHåndterer vedtaksHendelseHåndterer) {
+        this.topic = topicName;
+
+        final Consumed<String, String> consumed = Consumed.with(Topology.AutoOffsetReset.EARLIEST);
+
+        final var builder = new StreamsBuilder();
+        builder.stream(topic, consumed).foreach(vedtaksHendelseHåndterer::handleMessage);
+
+        this.stream = new KafkaStreams(builder.build(), KafkaProperties.forStreamsStringValue(APPLICATION_ID));
     }
+
+    private void addShutdownHooks() {
+        stream.setStateListener((newState, oldState) -> {
+            LOG.info("{} :: From state={} to state={}", topic, oldState, newState);
+
+            if (newState == KafkaStreams.State.ERROR) {
+                // if the stream has died there is no reason to keep spinning
+                LOG.warn("{} :: No reason to keep living, closing stream", topic);
+                stop();
+            }
+        });
+        stream.setUncaughtExceptionHandler(ex -> {
+            LOG.error(topic + " :: Caught exception in stream, exiting", ex);
+            return SHUTDOWN_CLIENT;
+        });
+    }
+
 
     @Override
     public boolean isAlive() {
-        return kcm.allRunning();
+        return stream != null && stream.state().isRunningOrRebalancing();
     }
 
     @Override
@@ -38,13 +75,15 @@ public class VedtakConsumer implements LiveAndReadinessAware, Controllable {
 
     @Override
     public void start() {
-        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
-        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
+        addShutdownHooks();
+        stream.start();
+        LOG.info("Starter konsumering av topic={}, tilstand={}", topic, stream.state());
     }
 
     @Override
     public void stop() {
-        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
-        kcm.stop();
+        LOG.info("Starter shutdown av topic={}, tilstand={} med 10 sekunder timeout", topic, stream.state());
+        stream.close(Duration.of(30, ChronoUnit.SECONDS));
+        LOG.info("Shutdown av topic={}, tilstand={} med 10 sekunder timeout", topic, stream.state());
     }
 }
