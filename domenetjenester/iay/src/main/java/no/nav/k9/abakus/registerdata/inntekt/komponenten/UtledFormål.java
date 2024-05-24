c44d26ad27c1e419331214752b95966c9d595315
Espen Velsvik <espenjv@users.noreply.github.com>
2024-05-24 08:25:14 +0200

    Bruker formål omsorgspenger for frisinn (#115)

--- domenetjenester/iay/src/main/java/no/nav/k9/abakus/registerdata/inntekt/komponenten/UtledFormål.java
+++ domenetjenester/iay/src/main/java/no/nav/k9/abakus/registerdata/inntekt/komponenten/UtledFormål.java
@@ -4,13 +4,13 @@ import no.nav.abakus.iaygrunnlag.kodeverk.YtelseType;
 
 class UtledFormål {
 
-
     static InntektsFormål utledFormålFraYtelse(YtelseType ytelse) {
         return switch (ytelse) {
             case OMSORGSPENGER -> InntektsFormål.FORMAAL_OMSORSGPENGER;
             case PLEIEPENGER_SYKT_BARN -> InntektsFormål.FORMAAL_PLEIEPENGER_SYKT_BARN;
             case PLEIEPENGER_NÆRSTÅENDE -> InntektsFormål.FORMAAL_PLEIEPENGER_NÆRSTÅENDE;
             case OPPLÆRINGSPENGER -> InntektsFormål.FORMAAL_OPPLÆRINGSPENGER;
+            case FRISINN -> InntektsFormål.FORMAAL_OMSORSGPENGER; // Har ikkje eget formål for frisinn
             default -> throw new IllegalArgumentException("Fant ingen passende formål for innhenting av inntekt for " + ytelse.getKode());
         };
     }
