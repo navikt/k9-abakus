d0598a7737fcbfe7fa8876dc56814e1deb04c155
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-08-18 16:06:37 +0200

    Opentelemetry i dev (#132)

---
+++ init-app.sh
@@ -0,0 +1,6 @@
+#start opentelemetry-agenten dersom konfigurert
+set +u
+if [ ! -z "${OTEL_EXPORTER_OTLP_ENDPOINT}" ]; then
+    echo "Configuring attatchment of opentelemetry-agent"
+    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/opentelemetry-javaagent.jar"
+fi
