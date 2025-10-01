6035fb26efd94e8eab718c85e810ca83f3ec0b3e
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-10-01 09:40:30 +0200

    Klargjør for kafka-endringer i vtp (#344)

--- felles/src/main/java/no/nav/k9/abakus/felles/kafka/KafkaProperties.java
+++ felles/src/main/java/no/nav/k9/abakus/felles/kafka/KafkaProperties.java
@@ -4,45 +4,27 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.UUID;
 
-import no.nav.k9.felles.konfigurasjon.env.Environment;
-
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.OffsetResetStrategy;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.Deserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.k9.felles.konfigurasjon.env.Environment;
 
 
 public class KafkaProperties {
 
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaProperties.class);
     private static final Environment ENV = Environment.current();
     private static final boolean IS_DEPLOYMENT = ENV.isProd() || ENV.isDev();
     private static final String APPLICATION_NAME = ENV.getRequiredProperty("NAIS_APP_NAME");
 
     private KafkaProperties() {
-    }
-
-    // Alle som produserer Json-meldinger
-    public static Properties forProducer() {
-
-        final Properties props = new Properties();
-        props.put(CommonClientConfigs.CLIENT_ID_CONFIG, generateClientId());
-        props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, getAivenConfig(AivenProperty.KAFKA_BROKERS));
-
-        putSecurity(props);
-        if (IS_DEPLOYMENT) {
-            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, getAivenConfig(AivenProperty.KAFKA_CREDSTORE_PASSWORD)); // Kun producer
-        }
-
-        // Serde
-        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return props;
     }
 
     public static <K,V> Properties forConsumerGenericValue(String groupId, Deserializer<K> keyDeserializer, Deserializer<V> valueDeserializer, OffsetResetStrategy offsetReset) {
@@ -72,18 +54,6 @@ public class KafkaProperties {
      * - Bør se på rocksdb-setting (se i historikk)
      */
 
-
-    // Trengs kun for de som skal konsumere Avro. Ellers ikke
-    public static String getAvroSchemaRegistryURL() {
-        return getAivenConfig(AivenProperty.KAFKA_SCHEMA_REGISTRY);
-    }
-
-    // Trengs kun for de som skal konsumere Avro. Ellers ikke
-    public static String getAvroSchemaRegistryBasicAuth() {
-        return getAivenConfig(AivenProperty.KAFKA_SCHEMA_REGISTRY_USER) + ":" + getAivenConfig(AivenProperty.KAFKA_SCHEMA_REGISTRY_PASSWORD);
-    }
-
-
     private static String getAivenConfig(AivenProperty property) {
         return Optional.ofNullable(ENV.getProperty(property.name()))
             .orElseGet(() -> ENV.getProperty(property.name().toLowerCase().replace('_', '.')));
@@ -94,7 +64,9 @@ public class KafkaProperties {
     }
 
     private static void putSecurity(Properties props) {
-        if (IS_DEPLOYMENT) {
+        boolean brukAivenPropetyLokalt = "true".equalsIgnoreCase(Environment.current().getProperty("KAFKA_BRUK_AIVEN_PROPERTY_LOKALT"));
+
+        if (IS_DEPLOYMENT || brukAivenPropetyLokalt) {
             var credStorePassword = getAivenConfig(AivenProperty.KAFKA_CREDSTORE_PASSWORD);
             props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name);
             props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "");
@@ -105,6 +77,7 @@ public class KafkaProperties {
             props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, getAivenConfig(AivenProperty.KAFKA_KEYSTORE_PATH));
             props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, credStorePassword);
         } else {
+            LOG.info("Bruker gammelt vtp oppsett.");
             props.setProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_SSL.name);
             props.setProperty(SaslConfigs.SASL_MECHANISM, "PLAIN");
             String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
