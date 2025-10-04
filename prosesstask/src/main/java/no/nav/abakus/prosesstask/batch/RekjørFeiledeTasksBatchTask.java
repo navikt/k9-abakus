56999391266e63e59123ac9213a2f815559037d8
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-10-04 09:55:00 +0200

    Oppdaterer k9-felles, k9-prosesstask. Flytter cron-uttrykk for batch-tasker (#351)

--- prosesstask/src/main/java/no/nav/abakus/prosesstask/batch/RekjørFeiledeTasksBatchTask.java
+++ prosesstask/src/main/java/no/nav/abakus/prosesstask/batch/RekjørFeiledeTasksBatchTask.java
@@ -1,20 +1,20 @@
 package no.nav.abakus.prosesstask.batch;
 
-import io.opentelemetry.instrumentation.annotations.WithSpan;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import no.nav.k9.prosesstask.api.BatchProsessTaskHandler;
 import no.nav.k9.prosesstask.api.ProsessTask;
 import no.nav.k9.prosesstask.api.ProsessTaskData;
-import no.nav.k9.prosesstask.api.ProsessTaskHandler;
 import no.nav.k9.prosesstask.api.ProsessTaskTjeneste;
+import no.nav.k9.prosesstask.impl.cron.CronExpression;
 
 @ApplicationScoped
-@ProsessTask(value = "retry.feilendeTasks", cronExpression = "0 30 6,8,10,12,14,16,18 * * *", maxFailedRuns = 1)
-public class RekjørFeiledeTasksBatchTask implements ProsessTaskHandler {
+@ProsessTask(value = "retry.feilendeTasks", maxFailedRuns = 1)
+public class RekjørFeiledeTasksBatchTask implements BatchProsessTaskHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(RekjørFeiledeTasksBatchTask.class);
     private ProsessTaskTjeneste taskTjeneste;
@@ -22,6 +22,11 @@ public class RekjørFeiledeTasksBatchTask implements ProsessTaskHandler {
     @Inject
     public RekjørFeiledeTasksBatchTask(ProsessTaskTjeneste taskTjeneste) {
         this.taskTjeneste = taskTjeneste;
+    }
+
+    @Override
+    public CronExpression getCron() {
+        return new CronExpression("0 30 6,8,10,12,14,16,18 * * *");
     }
 
     @WithSpan(value = "TASK retry.feilendeTasks")
