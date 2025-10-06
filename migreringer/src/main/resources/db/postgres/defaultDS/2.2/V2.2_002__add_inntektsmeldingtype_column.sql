cd64aea7151f44b27612af30dd6e0c77ea4730cd
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-10-06 10:36:16 +0200

    Fiks skriveleif

--- migreringer/src/main/resources/db/postgres/defaultDS/2.2/V2.2_002__add_inntektsmeldingtype_column.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.2/V2.2_002__add_inntektsmeldingtype_column.sql
@@ -1,5 +1,5 @@
 -- Add inntektsmeldingType column to iay_inntektsmelding table
-ALTER TABLE iay_inntektsmelding ADD COLUMN inntektsmeldingtype VARCHAR(50);
+ALTER TABLE iay_inntektsmelding ADD COLUMN inntektsmelding_type VARCHAR(50);
 
 -- Add comment for the new column
-COMMENT ON COLUMN iay_inntektsmelding.inntektsmeldingtype IS 'Type inntektsmelding';
+COMMENT ON COLUMN iay_inntektsmelding.inntektsmelding_type IS 'Type inntektsmelding';
