8508cd3b482fc31a2f57f1ed3d3be3af142eb802
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-10-16 14:06:30 +0200

    Fjerner InntektTjenesteV1

--- domenetjenester/iay/src/main/java/no/nav/k9/abakus/registerdata/inntekt/komponenten/InntektTjeneste.java
+++ domenetjenester/iay/src/main/java/no/nav/k9/abakus/registerdata/inntekt/komponenten/InntektTjeneste.java
@@ -1,44 +1,36 @@
 package no.nav.k9.abakus.registerdata.inntekt.komponenten;
 
-import static no.nav.k9.abakus.registerdata.inntekt.komponenten.UtledFormål.utledFormålFraYtelse;
-
-import java.net.URI;
-import java.time.YearMonth;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-
-import no.nav.k9.abakus.felles.samtidighet.UncheckedInterruptException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.abakus.iaygrunnlag.kodeverk.InntektskildeType;
 import no.nav.abakus.iaygrunnlag.kodeverk.YtelseType;
-import no.nav.k9.felles.exception.IntegrasjonException;
+import no.nav.k9.abakus.felles.jpa.IntervallEntitet;
+import no.nav.k9.abakus.typer.AktørId;
 import no.nav.k9.felles.exception.TekniskException;
 import no.nav.k9.felles.integrasjon.rest.ScopedRestIntegration;
 import no.nav.k9.felles.integrasjon.rest.SystemUserOidcRestClient;
 import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
-import no.nav.tjenester.aordningen.inntektsinformasjon.Aktoer;
-import no.nav.tjenester.aordningen.inntektsinformasjon.ArbeidsInntektIdent;
-import no.nav.tjenester.aordningen.inntektsinformasjon.ArbeidsInntektMaaned;
-import no.nav.tjenester.aordningen.inntektsinformasjon.Tilleggsinformasjon;
-import no.nav.tjenester.aordningen.inntektsinformasjon.inntekt.Inntekt;
-import no.nav.tjenester.aordningen.inntektsinformasjon.inntekt.InntektType;
-import no.nav.tjenester.aordningen.inntektsinformasjon.request.HentInntektListeBolkRequest;
-import no.nav.tjenester.aordningen.inntektsinformasjon.response.HentInntektListeBolkResponse;
-import no.nav.tjenester.aordningen.inntektsinformasjon.tilleggsinformasjondetaljer.Etterbetalingsperiode;
-import no.nav.tjenester.aordningen.inntektsinformasjon.tilleggsinformasjondetaljer.TilleggsinformasjonDetaljerType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static no.nav.k9.abakus.registerdata.inntekt.komponenten.UtledFormål.utledFormålFraYtelse;
 
 @ApplicationScoped
-@ScopedRestIntegration(scopeKey = "hentinntektlistebolk.scope", defaultScope = "api://prod-fss.team-inntekt.ikomp/.default")
+@ScopedRestIntegration(scopeKey = "inntektskomponenten.scope", defaultScope = "api://prod-fss.team-inntekt.ikomp/.default")
 public class InntektTjeneste {
 
     // Dato for eldste request til inntk - det er av og til noen ES saker som spør lenger tilbake i tid
@@ -49,15 +41,11 @@ public class InntektTjeneste {
     private static final Logger LOG = LoggerFactory.getLogger(InntektTjeneste.class);
 
     private Map<InntektskildeType, InntektsFilter> kildeTilFilter;
+    private Map<InntektsFilter, InntektskildeType> filterTilKilde;
 
 
     private SystemUserOidcRestClient oidcRestClient;
     private String url;
-
-    //bruker semafor for å begrense antall samtidige kall til inntektskomponenten
-    //regner ikke med å ha så mange i praksis pga bare 3 tråder brukes for å kjøre tasker, og hver av disse kaller inntil en håndfull ganger
-    private Semaphore semaphore = new Semaphore(15);
-
 
     InntektTjeneste() {
         // For CDI proxy
@@ -65,163 +53,132 @@ public class InntektTjeneste {
 
     @Inject
     public InntektTjeneste(SystemUserOidcRestClient oidcRestClient,
-                           @KonfigVerdi(value = "hentinntektlistebolk.url", defaultVerdi = "http://ikomp.team-inntekt/rs/api/v1/hentinntektlistebolk") String url) {
+                           @KonfigVerdi(value = "inntektskomponenten.hentinntektbulk.url", defaultVerdi = "http://ikomp.team-inntekt/rest/v2/inntekt/bulk") String url) {
         this.oidcRestClient = oidcRestClient;
         this.url = url;
         this.kildeTilFilter = Map.of(InntektskildeType.INNTEKT_OPPTJENING, InntektsFilter.OPPTJENINGSGRUNNLAG, InntektskildeType.INNTEKT_BEREGNING,
             InntektsFilter.BEREGNINGSGRUNNLAG, InntektskildeType.INNTEKT_SAMMENLIGNING, InntektsFilter.SAMMENLIGNINGSGRUNNLAG,
             InntektskildeType.INNTEKT_UNGDOMSYTELSE, InntektsFilter.UNGDOMSYTELSEGRUNNLAG);
+        this.filterTilKilde = kildeTilFilter.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
     }
 
-    public InntektsInformasjon finnInntekt(FinnInntektRequest finnInntektRequest, InntektskildeType kilde, YtelseType ytelse) {
-        HentInntektListeBolkResponse response = finnInntektRaw(finnInntektRequest, kilde, ytelse);
-        return oversettResponse(response, kilde, ytelse);
+    public Map<InntektskildeType, InntektsInformasjon> finnInntekt(AktørId aktørid,
+                                                                   YearMonthPeriode periode,
+                                                                   Set<InntektskildeType> kilder,
+                                                                   YtelseType ytelse) {
 
+        var filtre = kilder.stream().map(kildeTilFilter::get).filter(Objects::nonNull).map(InntektsFilter::getKode).toList();
+        YearMonth fomMåned = brukDato(periode.fom);
+        YearMonth tomMåned = brukDato(periode.tom);
+        var request = new InntektBulkApiInn(aktørid.getId(), filtre, utledFormålFraYtelse(ytelse).getKode(), fomMåned, tomMåned);
+        InntektBulkApiUt response = oidcRestClient.post(URI.create(url), request, InntektBulkApiUt.class);
+        return oversettResponse(response, kilder, ytelse);
     }
 
-    public HentInntektListeBolkResponse finnInntektRaw(FinnInntektRequest finnInntektRequest, InntektskildeType kilde, YtelseType ytelse) {
-        var request = lagRequest(finnInntektRequest, kilde, ytelse);
-        try {
-            boolean ledigPlass = semaphore.tryAcquire(30, TimeUnit.SECONDS);
-            if (!ledigPlass) {
-                throw new IllegalStateException("Fikk timeout på venting av semafor for kall til inntektskomponenten. Underøk om tjenesten er nede, vurder å øke antall tillatelser i semaforen.");
+    public record YearMonthPeriode(YearMonth fom, YearMonth tom) {
+        public YearMonthPeriode {
+            if (tom.isBefore(fom)){
+                throw new IllegalArgumentException("Feil i periode, tom kan ikke være før fom");
             }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new UncheckedInterruptException("En tråd ble interrupted mens den ventet på å få semafor",e);
-        }
-        try {
-            return oidcRestClient.post(URI.create(url), request, HentInntektListeBolkResponse.class);
-        } catch (RuntimeException e) {
-            throw new IntegrasjonException("FP-824246", "Feil ved kall til inntektstjenesten. Meld til #team_registre og #produksjonshendelser hvis dette skjer over lengre tidsperiode.", e);
-        } finally {
-            semaphore.release();
         }
     }
 
-    private HentInntektListeBolkRequest lagRequest(FinnInntektRequest finnInntektRequest, InntektskildeType kilde, YtelseType ytelse) {
-        var request = new HentInntektListeBolkRequest();
 
-        if (finnInntektRequest.getFnr() != null) {
-            request.setIdentListe(Collections.singletonList(Aktoer.newNaturligIdent(finnInntektRequest.getFnr())));
-        } else {
-            request.setIdentListe(Collections.singletonList(Aktoer.newAktoerId(finnInntektRequest.getAktørId())));
-        }
-
-        InntektsFilter filter = getFilter(kilde);
-        if (filter != null) {
-            request.setAinntektsfilter(filter.getKode());
-            request.setFormaal(filter.getFormål().orElse(utledFormålFraYtelse(ytelse)).getKode());
-        }
-        request.setMaanedFom(finnInntektRequest.getFom().isAfter(INNTK_TIDLIGSTE_DATO) ? finnInntektRequest.getFom() : INNTK_TIDLIGSTE_DATO);
-        request.setMaanedTom(finnInntektRequest.getTom().isAfter(INNTK_TIDLIGSTE_DATO) ? finnInntektRequest.getTom() : INNTK_TIDLIGSTE_DATO);
-        return request;
+    private static YearMonth brukDato(YearMonth dato) {
+        return dato != null && dato.isAfter(INNTK_TIDLIGSTE_DATO) ? dato : INNTK_TIDLIGSTE_DATO;
     }
 
+    private Map<InntektskildeType, InntektsInformasjon> oversettResponse(InntektBulkApiUt response,
+                                                                         Set<InntektskildeType> kilder,
+                                                                         YtelseType ytelse) {
 
-    private InntektsFilter getFilter(InntektskildeType kilde) {
-        // Skal bare få en verdi.
-        return kildeTilFilter.getOrDefault(kilde, null);
-    }
-
-    private InntektsInformasjon oversettResponse(HentInntektListeBolkResponse response, InntektskildeType kilde, YtelseType ytelse) {
-        if (response.getSikkerhetsavvikListe() != null && !response.getSikkerhetsavvikListe().isEmpty()) {
-            throw new IntegrasjonException("FP-535194",
-                String.format("Fikk følgende sikkerhetsavvik ved kall til inntektstjenesten: %s.", byggSikkerhetsavvikString(response)));
+        List<InntektBulk> svar = Optional.ofNullable(response).map(InntektBulkApiUt::bulk).orElseGet(List::of);
+        if (svar.isEmpty()) {
+            return Map.of();
         }
 
+        var oversatt = new ArrayList<InntektsInformasjon>();
+        for (var i : svar) {
+            var kilde = filterTilKilde.getOrDefault(getFilter(i), InntektskildeType.UDEFINERT);
+            if (!kilder.contains(kilde)) {
+                continue;
+            }
+            var data = Optional.ofNullable(i.data()).orElseGet(List::of);
+            List<Månedsinntekt> månedsinntekter = new ArrayList<>();
+            for (var d : data) {
+                månedsinntekter.addAll(oversettInntekter(d, kilde));
+            }
+            oversatt.add(new InntektsInformasjon(månedsinntekter, kilde));
+        }
+
+        return oversatt.stream().collect(Collectors.toMap(InntektsInformasjon::getKilde, i -> i));
+    }
+
+    private List<Månedsinntekt> oversettInntekter(Inntektsinformasjon inntektsinformasjon, InntektskildeType kilde) {
         List<Månedsinntekt> månedsinntekter = new ArrayList<>();
-
-        List<ArbeidsInntektIdent> arbeidsInntektIdentListe = response.getArbeidsInntektIdentListe();
-        if (response.getArbeidsInntektIdentListe() != null) {
-            for (var arbeidsInntektIdent : arbeidsInntektIdentListe) {
-                if (arbeidsInntektIdent.getArbeidsInntektMaaned() != null) {
-                    for (ArbeidsInntektMaaned arbeidsInntektMaaned : arbeidsInntektIdent.getArbeidsInntektMaaned()) {
-                        oversettInntekter(månedsinntekter, arbeidsInntektMaaned, kilde);
-                    }
-                }
+        var inntekter = Optional.ofNullable(inntektsinformasjon.inntektListe()).orElseGet(List::of);
+        for (var inntekt : inntekter) {
+            var brukYM = inntektsinformasjon.maaned();
+            var tilleggsinformasjon = inntekt.tilleggsinformasjon();
+            if (tilleggsinformasjon != null && "YtelseFraOffentlige".equals(inntekt.type()) && "Etterbetalingsperiode".equals(
+                tilleggsinformasjon.type()) && tilleggsinformasjon.startdato() != null && skalPeriodisereInntektsKilde(kilde)) {
+                brukYM = YearMonth.from(tilleggsinformasjon.startdato().plusDays(1));
             }
-        }
-        return new InntektsInformasjon(månedsinntekter, kilde);
-    }
+            var månedsinntekt = new Månedsinntekt.Builder().medBeløp(inntekt.beloep()).medSkatteOgAvgiftsregelType(inntekt.skatteOgAvgiftsregel());
 
-    private void oversettInntekter(List<Månedsinntekt> månedsinntekter, ArbeidsInntektMaaned arbeidsInntektMaaned, InntektskildeType kilde) {
-        var arbeidsInntektInformasjon = arbeidsInntektMaaned.getArbeidsInntektInformasjon();
-
-        if (arbeidsInntektInformasjon != null && arbeidsInntektInformasjon.getInntektListe() != null) {
-            for (var inntekt : arbeidsInntektInformasjon.getInntektListe()) {
-                var brukYM = inntekt.getUtbetaltIMaaned();
-                var tilleggsinformasjon = inntekt.getTilleggsinformasjon();
-                if (erYtelseFraOffentlig(inntekt) && erEtterbetaling(tilleggsinformasjon) && skalPeriodisereInntektsKilde(kilde)) {
-                    brukYM = YearMonth.from(
-                        ((Etterbetalingsperiode) tilleggsinformasjon.getTilleggsinformasjonDetaljer()).getEtterbetalingsperiodeFom().plusDays(1));
-                }
-                var månedsinntekt = new Månedsinntekt.Builder().medBeløp(inntekt.getBeloep())
-                    .medSkatteOgAvgiftsregelType(inntekt.getSkatteOgAvgiftsregel());
-
-                if (brukYM != null) {
-                    månedsinntekt.medMåned(brukYM);
-                }
-                utledOgSettUtbetalerOgYtelse(inntekt, månedsinntekt);
-
-                månedsinntekter.add(månedsinntekt.build());
+            if (brukYM != null) {
+                månedsinntekt.medMåned(brukYM);
             }
+            utledOgSettUtbetalerOgYtelse(inntektsinformasjon, inntekt, månedsinntekt);
+
+            månedsinntekter.add(månedsinntekt.build());
+
         }
+        return månedsinntekter;
     }
 
     private boolean skalPeriodisereInntektsKilde(InntektskildeType kilde) {
         return SKAL_PERIODISERE_INNTEKTSKILDE.contains(kilde);
     }
 
-    private boolean erEtterbetaling(Tilleggsinformasjon tilleggsinformasjon) {
-        return tilleggsinformasjon != null && TilleggsinformasjonDetaljerType.ETTERBETALINGSPERIODE.equals(
-            tilleggsinformasjon.getTilleggsinformasjonDetaljer().getDetaljerType());
-    }
-
-    private void utledOgSettUtbetalerOgYtelse(Inntekt inntekt, Månedsinntekt.Builder månedsinntekt) {
-        if (erYtelseFraOffentlig(inntekt)) {
-            månedsinntekt.medYtelse(true).medYtelseKode(inntekt.getBeskrivelse());
-        } else if (erPensjonEllerTrygd(inntekt)) {
-            månedsinntekt.medYtelse(true).medPensjonEllerTrygdKode(inntekt.getBeskrivelse());
-        } else if (erNæringsinntekt(inntekt)) {
-            månedsinntekt.medYtelse(true).medNæringsinntektKode(inntekt.getBeskrivelse());
-        } else if (erLønn(inntekt)) {
-            månedsinntekt.medYtelse(false);
-            månedsinntekt.medArbeidsgiver(inntekt.getVirksomhet().getIdentifikator()); // OK med NPE hvis inntekt.getArbeidsgiver() er null
-            månedsinntekt.medArbeidsforholdRef(inntekt.getArbeidsforholdREF());
-            månedsinntekt.medLønnsbeskrivelseKode(inntekt.getBeskrivelse());
-        } else {
-            throw new TekniskException("FP-711674", String.format("Kunne ikke mappe svar fra Inntektskomponenten: virksomhet=%s, inntektType=%s",
-                inntekt.getVirksomhet().getIdentifikator(), inntekt.getInntektType()));
-        }
-    }
-
-    private boolean erLønn(Inntekt inntekt) {
-        return InntektType.LOENNSINNTEKT.equals(inntekt.getInntektType());
-    }
-
-    private boolean erYtelseFraOffentlig(Inntekt inntekt) {
-        return InntektType.YTELSE_FRA_OFFENTLIGE.equals(inntekt.getInntektType());
-    }
-
-    private boolean erPensjonEllerTrygd(Inntekt inntekt) {
-        return InntektType.PENSJON_ELLER_TRYGD.equals(inntekt.getInntektType());
-    }
-
-    private boolean erNæringsinntekt(Inntekt inntekt) {
-        return InntektType.NAERINGSINNTEKT.equals(inntekt.getInntektType());
-    }
-
-    private String byggSikkerhetsavvikString(HentInntektListeBolkResponse response) {
-        var stringBuilder = new StringBuilder();
-        var sikkerhetsavvikListe = response.getSikkerhetsavvikListe();
-        if (sikkerhetsavvikListe != null && !sikkerhetsavvikListe.isEmpty()) {
-            stringBuilder.append(sikkerhetsavvikListe.get(0).getTekst());
-            for (int i = 1; i < sikkerhetsavvikListe.size(); i++) {
-                stringBuilder.append(", ");
-                stringBuilder.append(sikkerhetsavvikListe.get(i).getTekst());
+    private void utledOgSettUtbetalerOgYtelse(Inntektsinformasjon inntektsinformasjon, Inntekt inntekt, Månedsinntekt.Builder månedsinntekt) {
+        switch (inntekt.type()) {
+            case "YtelseFraOffentlige" -> månedsinntekt.medYtelse(true).medYtelseKode(inntekt.beskrivelse());
+            case "PensjonEllerTrygd" -> månedsinntekt.medYtelse(true).medPensjonEllerTrygdKode(inntekt.beskrivelse());
+            case "Naeringsinntekt" -> månedsinntekt.medYtelse(true).medNæringsinntektKode(inntekt.beskrivelse());
+            case "Loennsinntekt" -> {
+                månedsinntekt.medYtelse(false);
+                månedsinntekt.medArbeidsgiver(inntektsinformasjon.underenhet());
+                månedsinntekt.medLønnsbeskrivelseKode(inntekt.beskrivelse());
             }
+            case null, default -> throw new TekniskException("FP-711674",
+                String.format("Kunne ikke mappe svar fra Inntektskomponenten: virksomhet=%s, inntektType=%s", inntektsinformasjon.underenhet(),
+                    inntekt.type()));
         }
-        return stringBuilder.toString();
+    }
+
+    private static InntektsFilter getFilter(InntektBulk bulk) {
+        return Arrays.stream(InntektsFilter.values())
+            .filter(f -> f.getKode().equals(bulk.filter()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException(String.format("Ugyldig filter i ikomp-respons %s ", bulk.filter())));
+    }
+
+    public record InntektBulkApiInn(String personident, List<String> filter, String formaal, YearMonth maanedFom, YearMonth maanedTom) {
+    }
+
+    public record InntektBulkApiUt(List<InntektBulk> bulk) {
+    }
+
+    public record InntektBulk(String filter, List<Inntektsinformasjon> data) {
+    }
+
+    public record Inntektsinformasjon(YearMonth maaned, String opplysningspliktig, String underenhet, List<Inntekt> inntektListe) {
+    }
+
+    public record Inntekt(String type, BigDecimal beloep, String beskrivelse, String skatteOgAvgiftsregel, Tilleggsinformasjon tilleggsinformasjon) {
+    }
+
+    public record Tilleggsinformasjon(String type, LocalDate startdato, LocalDate sluttdato) {
     }
 }
