e78c6fda585c7444e034332384b09f2eef838f57
inga <inga.kristine.holen@nav.no>
2026-02-12 10:16:56 +0100

    Legger til fagsystem for dp-sak også

--- kodeverk/src/main/java/no/nav/abakus/iaygrunnlag/kodeverk/Fagsystem.java
+++ kodeverk/src/main/java/no/nav/abakus/iaygrunnlag/kodeverk/Fagsystem.java
@@ -24,6 +24,7 @@ public enum Fagsystem implements Kodeverdi {
     INFOTRYGD("INFOTRYGD", "Infotrygd", "IT01"),
     ARENA("ARENA", "Arena", "AO01"),
     KELVIN("KELVIN", "Kelvin", "KELVIN"),
+    DPSAK("DPSAK", "dp-sak", "DPSAK"),
     INNTEKT("INNTEKT", "INNTEKT", "FS28"),
     MEDL("MEDL", "MEDL", "FS18"),
     GOSYS("GOSYS", "Gosys", "FS22"),
